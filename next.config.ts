edd93f2380e996116cb34e1c6eb0b6e672152e25
[dyad] <joshuanesbit@gmail.com>
2025-08-21 23:10:10 -0700

    [dyad] Reverted internationalization changes to fix socket hangup error - wrote 2 file(s), deleted 8 file(s)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
@@ -17,4 +14,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
