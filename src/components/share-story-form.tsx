32d85eaf5c66811e04a6d60450b7230d76bfd868
[dyad] <joshuanesbit@gmail.com>
2025-08-29 16:25:01 -0700

    [dyad] Fixed build error by adding 'neighborhood' to form data - wrote 1 file(s)

--- src/components/share-story-form.tsx
+++ src/components/share-story-form.tsx
@@ -19,6 +19,7 @@ export function ShareStoryForm({ onClose }: ShareStoryFormProps) {
     title: "",
     briefIntro: "",
     author: "",
+    neighborhood: "",
     language: "",
     translatorAvailable: false,
     translatorLanguage: "",
